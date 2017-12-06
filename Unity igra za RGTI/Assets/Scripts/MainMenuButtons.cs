67505e4e53ee65afd88bf4b23b1b4a782f19da0b
Aleksander Kovač <aleksander.kovac97@hotmail.com>
2017-12-06 23:42:24 +0100

    1. commit
    Izdelan  osnovni meni za našo igro,
    Meni To-Do:
    -uredi slider za glasbo
    -dodaj neko test glasbo za preizkušanje sliderja
    Game:
    -ustvari objekte v game sceni

---
+++ Unity igra za RGTI/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    //funkcionalnost za Glavni meni
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Igra končana");
+        Application.Quit();
+    }
+    
+}
---
+++ Unity igra za RGTI/Assets/Scripts/OptionsButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsButton : MonoBehaviour {
+
+    public int PercetForVolume;
+    public Slider MainSlider;
+
+
+    // Use this for initialization
+    void Start() {
+        PercetForVolume = (int)GameObject.Find("MainSlider").GetComponent<Slider>().value;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void ValueChangeCheck()
+    {
+        Debug.Log((GameObject.Find("MainSlider").GetComponent<Slider>().value).ToString());
+    }
+}
